1ddbaacc417cfa3ef796e746a78d09fc6f8d8ab2
unknown <flowslowgrow@yandex.ru>
2022-10-17 21:00:13 +0500

    infra_v1.5

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),

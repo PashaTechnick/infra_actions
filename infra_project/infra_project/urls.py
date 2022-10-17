070eb792fb795f1fd0e1291388577a40e80e763e
unknown <flowslowgrow@yandex.ru>
2022-10-17 20:54:23 +0500

    infra_v1.4

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('infra_app.urls', namespace='infra_app')),
+    path('admin/', admin.site.urls),
 ]
